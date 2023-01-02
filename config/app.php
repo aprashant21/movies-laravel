94ad09a9c5e5a01fab9ba7bf1a54a41c9ba63b33
aprashant21 <aprashant21@tbc.edu.np>
2023-01-02 09:30:29 +0545

    some changes 8

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://the-aan-movie.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
